1da183fa931dd7917c7beba2aa4cc7ed0213aae1
Alyson Orta <alyson.orta@gmail.com>
2021-06-02 15:26:52 -0400

    adding initial iteration of function to change colors of rows depending on time

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 var today = moment() ;
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
+
+var timeblocks = [document.getElementById("8"), document.getElementById("9"), document.getElementById("10"), document.getElementById("11"), document.getElementById("12")];
+var currentTime = moment().format("H");
+
+
+// Function to color code time blocks depending on past, present, future
+
+function compareTime () {
+    var rowId = timeblocks.row.id, rowHour;
+    if (rowId) {
+        rowHour = parseInt(rowID);
+    } 
+    if (rowHour) {
+        if (currentTime === rowHour) {
+            changeColor(row, "green");
+        } else if ((currentTime < rowHour) && (currentTime > rowHour - 6)) {
+            changeColor(row, "lightblue");
+        } else if ((currentTime > rowHour) && (currentTime < rowHour + 6)) {
+            changeColor(row, "pink");
+        } else {
+            changeColor(row, "black");
+        }
+    }
+};
+
+function changeColor(element, color) {
+    element.style.backgroundColor = color;
+}
