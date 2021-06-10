0b37b53efd83af79da51e61548ca5f13f9f61a61
Alyson Orta <alyson.orta@gmail.com>
2021-06-10 15:08:04 -0400

    Updated task inputs and save buttons to include all rows and added code to change color of rows (currently not working)

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # keep-me-organized-scheduler
+Keep Me Organized Scheduler:
+
+
+
 
 References:
-<link src = "https://getbootstrap.com/"></link>
-<link src = "https://momentjs.com/">
+<link src = "https://getbootstrap.com/"> Bootstrap </link>
+<link src = "https://momentjs.com/">Moment JS</link>
+<link src = "https://www.w3schools.com/>">W3 Schools </link>
+<link src = "https://stackoverflow.com/>">Stack Overflow </link>
--- script.js
+++ script.js
@@ -1,44 +1,150 @@
+// Adding variable for date and add specific format
 var today = moment() ;
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
-var timeRow = [
-    8,9,10,11,12,13,14,15,16,17
+//Adding array variable to get time rows from html
+var timeblocks = [
+    document.getElementById("8"), 
+    document.getElementById("9"), 
+    document.getElementById("10"), 
+    document.getElementById("11"), 
+    document.getElementById("12"),
+    document.getElementById("13"),
+    document.getElementById("14"),
+    document.getElementById("15"),
+    document.getElementById("16"),
+    document.getElementById("17")
 ];
+
+console.log(timeblocks);
+
+
+//Adding variable to call current time and format same as timeblocks 
 var currentTime = moment().format("H");
 
-console.log(timeRow);
+//Adding function for changing row color - currently not working
+function changeRowColor () {
+    for (var i = 0; i<10; i++) {
+        if (timeblocks[i] == currentTime) {
+            timeblocks.style.backgroundcolor= "#ff6961";
+        } else if (timeblocks[i] == currentTime - 6) {
+            timeblocks.style.backgroundcolor= "#d3d3d3";
+        } else if (timeblocks[i] == currentTime +6) {
+            timeblocks.style.backgroundcolor= "#77dd77";
+        }
+    }
+}
 
-// if (timeRow[0] == currentTime) {
-//     console.log("8");
-// } else if (timeRow[1] == currentTime) {
-//     console.log("9");
-// } else {
-//     console.log("This isn't working!");
-// }
+changeRowColor(timeblocks);
 
+// Adding variables and functions for input fields and save buttons to save to local storage on the click of the save button
 var saveButton8 = document.getElementById("saveButton8");
-var inputField8 = document.getElementById("8");
+var inputField8 = document.getElementById("8input");
 
 
 saveButton8.addEventListener("click", function () {
 
     if (inputField8) {
-        localStorage.setItem("Task", inputField8.value)
+        localStorage.setItem("Task 8", inputField8.value)
+    }
+})
+
+var saveButton9 = document.getElementById("saveButton9");
+var inputField9 = document.getElementById("9input");
+
+
+saveButton9.addEventListener("click", function () {
+
+    if (inputField9) {
+        localStorage.setItem("Task 9", inputField9.value)
+    }
+})
+var saveButton10 = document.getElementById("saveButton10");
+var inputField10 = document.getElementById("10input");
+
+
+saveButton10.addEventListener("click", function () {
+
+    if (inputField10) {
+        localStorage.setItem("Task 10", inputField10.value)
+    }
+})
+
+var saveButton11 = document.getElementById("saveButton11");
+var inputField11 = document.getElementById("11input");
+
+
+saveButton11.addEventListener("click", function () {
+
+    if (inputField11) {
+        localStorage.setItem("Task 11", inputField11.value)
+    }
+})
+
+var saveButton12 = document.getElementById("saveButton12");
+var inputField12 = document.getElementById("12input");
+
+
+saveButton12.addEventListener("click", function () {
+
+    if (inputField12) {
+        localStorage.setItem("Task 12", inputField12.value)
+    }
+})
+var saveButton13 = document.getElementById("saveButton13");
+var inputField13 = document.getElementById("13input");
+
+
+saveButton13.addEventListener("click", function () {
+
+    if (inputField13) {
+        localStorage.setItem("Task 13", inputField13.value)
+    }
+})
+
+var saveButton14 = document.getElementById("saveButton14");
+var inputField14 = document.getElementById("14input");
+
+
+saveButton14.addEventListener("click", function () {
+
+    if (inputField14) {
+        localStorage.setItem("Task 14", inputField14.value)
+    }
+})
+
+var saveButton15 = document.getElementById("saveButton15");
+var inputField15 = document.getElementById("15input");
+
+
+saveButton15.addEventListener("click", function () {
+
+    if (inputField15) {
+        localStorage.setItem("Task 15", inputField15.value)
+    }
+})
+
+var saveButton16 = document.getElementById("saveButton16");
+var inputField16 = document.getElementById("16input");
+
+
+saveButton16.addEventListener("click", function () {
+
+    if (inputField16) {
+        localStorage.setItem("Task 16", inputField16.value)
+    }
+})
+
+var saveButton17 = document.getElementById("saveButton17");
+var inputField17 = document.getElementById("17input");
+
+
+saveButton17.addEventListener("click", function () {
+
+    if (inputField17) {
+        localStorage.setItem("Task 17", inputField17.value)
     }
 })
 
 
-// var timeblocks = [
-//     document.getElementById("#8"), 
-//     document.getElementById("#9"), 
-//     document.getElementById("#10"), 
-//     document.getElementById("#11"), 
-//     document.getElementById("#12"),
-//     document.getElementById("#13"),
-//     document.getElementById("#14"),
-//     document.getElementById("#15"),
-//     document.getElementById("#16"),
-//     document.getElementById("#17")
-// ];
 
-// console.log(timeRow);
